c949aee4f27844f7145dcf223b6e88d52f2c28c3
Tobin Saunders <tobin.saunders@burberry.com>
2025-01-03 14:40:29 +0000

    honeypot input and remove onchange for contact form

--- src/components/Sections/Contact/ContactForm.tsx
+++ src/components/Sections/Contact/ContactForm.tsx
@@ -1,34 +1,34 @@
 import {FC, memo, useCallback, useMemo, useState} from 'react';
 
-interface FormData {
-  name: string;
-  email: string;
-  message: string;
-}
+// interface FormData {
+//   name: string;
+//   email: string;
+//   message: string;
+// }
 
 const ContactForm: FC = memo(() => {
-  const defaultData = useMemo(
-    () => ({
-      name: '',
-      email: '',
-      message: '',
-    }),
-    [],
-  );
+  // const defaultData = useMemo(
+  //   () => ({
+  //     name: '',
+  //     email: '',
+  //     message: '',
+  //   }),
+  //   [],
+  // );
 
-  const [data, setData] = useState<FormData>(defaultData);
+  // const [data, setData] = useState<FormData>(defaultData);
   const [messageSent, setMessageSent] = useState(false);
 
-  const onChange = useCallback(
-    <T extends HTMLInputElement | HTMLTextAreaElement>(event: React.ChangeEvent<T>): void => {
-      const {name, value} = event.target;
+  // const onChange = useCallback(
+  //   <T extends HTMLInputElement | HTMLTextAreaElement>(event: React.ChangeEvent<T>): void => {
+  //     const {name, value} = event.target;
 
-      const fieldData: Partial<FormData> = {[name]: value};
+  //     const fieldData: Partial<FormData> = {[name]: value};
 
-      setData({...data, ...fieldData});
-    },
-    [data],
-  );
+  //     setData({...data, ...fieldData});
+  //   },
+  //   [data],
+  // );
 
   const inputClasses =
     'bg-neutral-700 border-0 focus:border-0 focus:outline-none focus:ring-1 focus:ring-orange-600 rounded-md placeholder:text-neutral-400 placeholder:text-sm text-neutral-200 text-sm';
@@ -40,25 +40,10 @@ const ContactForm: FC = memo(() => {
       name="contact"
       onSubmit={() => setMessageSent(!messageSent)}
       data-netlify="true">
-      <input className={inputClasses} name="name" onChange={onChange} placeholder="Name" required type="text" />
-      <input
-        autoComplete="email"
-        className={inputClasses}
-        name="email"
-        onChange={onChange}
-        placeholder="Email"
-        required
-        type="email"
-      />
-      <textarea
-        className={inputClasses}
-        maxLength={250}
-        name="message"
-        onChange={onChange}
-        placeholder="Message"
-        required
-        rows={6}
-      />
+      <input className={inputClasses} name="name" placeholder="Name" required type="text" />
+      <input autoComplete="email" className={inputClasses} name="email" placeholder="Email" required type="email" />
+      <textarea className={inputClasses} maxLength={250} name="message" placeholder="Message" required rows={6} />
+      <input type="hidden" name="form-name" value="contact" />
       <button
         aria-label="Submit contact form"
         className="w-max rounded-full border-2 border-orange-600 bg-stone-900 px-4 py-2 text-sm font-medium text-white shadow-md outline-none hover:bg-stone-800 focus:ring-2 focus:ring-orange-600 focus:ring-offset-2 focus:ring-offset-stone-800"
